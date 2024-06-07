b20f7ce858e05a3f7bed0931e995ac17f4e2e8e5
cambrian-X <cmurphy@duck.com>
2024-06-07 16:42:31 -0500

    Removed space

--- webpage_load.py
+++ webpage_load.py
@@ -2,7 +2,7 @@
 import requests
 from browser import headers
 
-link= input("Enter link address:  ")
+link= input("Enter link address: ")
 source=requests.get(link,headers=headers).text
 with open('webpage.html', "w", encoding="utf-8") as f:
     f.write(source)
